da08e1189b8dcd2805696ecaa0781691ddfda703
saqibbedar <Chandio.Saqib.M@gmail.com>
2024-05-18 17:44:24 +0500

    update

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,13 @@ require('dotenv').config()
 
 const app = express();
 
-app.use(cors());  
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-frontend.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 
 app.use(express.json());  
 
